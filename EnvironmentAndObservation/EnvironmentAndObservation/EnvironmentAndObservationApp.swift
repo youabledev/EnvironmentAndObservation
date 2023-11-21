5f79a511871606a51d44d2576c7b20c53083a7ee
youabledev <youable.framios@gmail.com>
2023-11-21 23:07:48 +0900

    add: project

---
+++ EnvironmentAndObservation/EnvironmentAndObservation/EnvironmentAndObservationApp.swift
@@ -0,0 +1,17 @@
+//
+//  EnvironmentAndObservationApp.swift
+//  EnvironmentAndObservation
+//
+//  Created by zumin you on 2023/11/21.
+//
+
+import SwiftUI
+
+@main
+struct EnvironmentAndObservationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
